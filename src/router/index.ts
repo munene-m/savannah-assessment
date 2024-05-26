1f84873494217799176f58ba4f32bcebffc1a27c
munene-m <munenenjue18@gmail.com>
2024-05-26 16:06:05 +0300

    chore: remove unused import

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,8 +6,6 @@ import {
 } from 'vue-router'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
-import HomeView from '../views/HomeView.vue'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
