d038c2bf927f91f0742ffe50cc6c08a1c5f24a73
munene-m <munenenjue18@gmail.com>
2024-05-28 00:46:48 +0300

    feat(album): this is the page shown when an album is selected. It displays an album's photos arranged in a grid.

--- src/views/AlbumView.vue
+++ src/views/AlbumView.vue
@@ -1,19 +1,59 @@
 <script setup>
 import axios from 'axios'
-import { onMounted, reactive, ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
-const album = reactive({})
-onMounted(async () => {
-  const response = await axios.get(`https://jsonplaceholder.typicode.com/albums/${id}`)
-  console.log(response.data)
-})
-defineProps({
+const props = defineProps({
   id: String
+})
+
+const album = ref(null)
+const albumPhotos = ref([])
+const loadingStates = ref([])
+
+onMounted(async () => {
+  try {
+    const albumResponse = await axios.get(`https://jsonplaceholder.typicode.com/albums/${props.id}`)
+    album.value = albumResponse.data
+
+    const photosResponse = await axios.get(
+      `https://jsonplaceholder.typicode.com/photos?albumId=${props.id}`
+    )
+    albumPhotos.value = photosResponse.data
+  } catch (error) {
+    console.error('Failed to fetch data:', error)
+  }
+  loadingStates.value = albumPhotos.value.map(() => true)
 })
 </script>
 
 <template>
-  <main>
-    <p>Album page</p>
+  <main class="container mx-auto py-8">
+    <div v-if="album" class="px-4 my-2">
+      <p class="text-center text-xl font-semibold mb-4">List of {{ album.title }}'s photos:</p>
+
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 justify-items-center lg:grid-cols-3 xl:grid-cols-4 gap-4 m-auto"
+      >
+        <div
+          class="my-2 p-3 rounded-lg shadow-lg flex flex-col items-center bg-white max-w-xs w-full"
+          v-for="photo in albumPhotos"
+          :key="photo.id"
+        >
+          <div class="flex items-center gap-2 justify-between mb-2 w-full">
+            <img
+              :src="photo.thumbnailUrl"
+              class="w-16 h-16 rounded-full object-cover"
+              alt="Thumbnail"
+            />
+            <p class="text-sm font-medium text-end">{{ photo.title }}</p>
+          </div>
+          <img :src="photo.url" class="rounded-md h-48 w-full object-cover mb-2" alt="Photo" />
+        </div>
+      </div>
+    </div>
+    <div v-else class="flex items-center flex-col justify-center mt-10 gap-2">
+      <p class="loading loading-dots loading-md text-center"></p>
+      <span>Loading album data...</span>
+    </div>
   </main>
 </template>
