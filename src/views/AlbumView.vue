16b4a33f5ed856a412ea574240d61e4a348d1ad6
munene-m <munenenjue18@gmail.com>
2024-06-02 15:59:11 +0300

    chore: move album content to its own card

--- src/views/AlbumView.vue
+++ src/views/AlbumView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import AlbumCard from '@/components/AlbumCard.vue'
 import axios from 'axios'
 import { onMounted, ref } from 'vue'
 
@@ -29,26 +30,14 @@ onMounted(async () => {
 <template>
   <main class="container mx-auto pb-8 pt-24">
     <div v-if="album" class="px-4 my-2">
-      <h1 class="text-xl mx-2 text-center font-bold">List of {{ album.title }}'s photos:</h1>
+      <h1 class="text-xl mx-2 text-center font-bold">
+        List of {{ album.title }}'s photos and its details:
+      </h1>
 
       <div
         class="grid grid-cols-1 sm:grid-cols-2 justify-items-center lg:grid-cols-3 xl:grid-cols-4 gap-4 m-auto"
       >
-        <div
-          class="my-2 p-3 rounded-lg shadow-lg flex flex-col items-center bg-white max-w-xs w-full"
-          v-for="photo in albumPhotos"
-          :key="photo.id"
-        >
-          <div class="flex items-center gap-2 justify-between mb-2 w-full">
-            <img
-              :src="photo.thumbnailUrl"
-              class="w-16 h-16 rounded-full object-cover"
-              alt="Thumbnail"
-            />
-            <p class="text-sm font-medium text-end">{{ photo.title }}</p>
-          </div>
-          <img :src="photo.url" class="rounded-md h-48 w-full object-cover mb-2" alt="Photo" />
-        </div>
+        <AlbumCard :albumPhotos="albumPhotos" />
       </div>
     </div>
     <div v-else class="flex items-center flex-col justify-center mt-10 gap-2 pt-24">
